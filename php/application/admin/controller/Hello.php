8bb1cfcb121c3f90e0483e3ae01de9937185f994
yuejiaming <13120314832@163.com>
2017-12-01 18:41:23 +0800

    VueThink
    VueThink开源的前后端分离项目

---
+++ php/application/admin/controller/Hello.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace app\admin\controller;
+
+use think\Request;
+use think\Db;
+use app\common\adapter\AuthAdapter;
+use app\common\controller\Common;
+
+
+class Hello extends Common
+{
+    public function  index()
+    {
+
+        echo "Hello Vuethink";
+    }
+}
